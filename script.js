6f8a20372e6efe23568d0627d79f59932484c737
kenhardika <kenzamahardika@gmail.com>
2022-01-17 10:20:21 +0700

    add js and link css

---
+++ script.js
@@ -0,0 +1,9 @@
+function computerPlay(rpsArray) { 
+    return computerRandom = rpsArray[Math.floor(Math.random()*rpsArray.length)]; 
+}
+computerPlay( rpsArray= [
+        "Rock",
+        "Paper",
+        "Scissors"
+    ]);
+console.log(computerRandom);
