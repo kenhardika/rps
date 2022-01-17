821e1a7241a779ae7d74b95afff380af1627ac34
kenhardika <kenzamahardika@gmail.com>
2022-01-17 11:28:37 +0700

    add button and simple conditionals

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+const playerChoice = document.getElementById('playerChoice');
+const startGame = document.querySelector('.startGame')
+
 function computerPlay(rpsArray) { 
-    return computerRandom = rpsArray[Math.floor(Math.random()*rpsArray.length)]; 
-}
+        return computerRandom = rpsArray[Math.floor(Math.random()*rpsArray.length)]; 
+    }
 computerPlay( rpsArray= [
-        "Rock",
-        "Paper",
-        "Scissors"
-    ]);
+            "Rock",
+            "Paper",
+            "Scissors"
+        ]);
 console.log(computerRandom);
+
+startGame.addEventListener('click', result);
+
+function result() {
+    const choice = playerChoice.value;
+
+    if (choice === "Rock") {
+    console.log("your choice is rock")
+    }
+    else {
+    console.log("ah you're choosing another thing")
+    }
+    return result
+}
