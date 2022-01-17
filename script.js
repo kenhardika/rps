08b6764e0338a5ceca7914df61335bd258e44328
kenhardika <kenzamahardika@gmail.com>
2022-01-17 19:19:47 +0700

    finished the rps function, works

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playerChoice = document.getElementById('playerChoice');
 const startGame = document.querySelector('.startGame')
 
 function computerPlay(rpsArray) { 
-        return computerRandom = rpsArray[Math.floor(Math.random()*rpsArray.length)]; 
+        return computerRandom = rpsArray[Math.floor(Math.random()*rpsArray.length)].toLowerCase(); 
     }
 computerPlay( rpsArray= [
             "Rock",
@@ -14,13 +14,38 @@ console.log(computerRandom);
 startGame.addEventListener('click', result);
 
 function result() {
-    const choice = playerChoice.value;
+    const playerSelection = playerChoice.value.toLowerCase();
 
-    if (choice === "Rock") {
-    console.log("your choice is rock")
+    if (playerSelection === "rock" && computerRandom === "rock") {
+    console.log("Both choose Rock, so Draw")
     }
-    else {
-    console.log("ah you're choosing another thing")
+    else if (playerSelection === "rock" && computerRandom === "paper") {
+    console.log("ah rock lose against paper") 
+    }
+    else if (playerSelection === "rock" && computerRandom === "scissors") {
+    console.log("ROCK WIN AGAINST SCISSORS")
+    }
+    else if (playerSelection === "paper" && computerRandom === "rock") {
+    console.log("PAPER WIN AGAINST ROCK")
+    } 
+    else if (playerSelection === "paper" && computerRandom === "paper") {
+    console.log("Both choose Paper, so Draw")
+    }
+    else if (playerSelection === "paper" && computerRandom === "scissors") {
+    console.log("ah paper lose against scissors")
+    }
+    else if (playerSelection === "scissors" && computerRandom === "rock") {
+    console.log("ah scissors lose against rock")
+    }
+    else if (playerSelection === "scissors" && computerRandom === "paper") {
+    console.log("SCISSORS WIN AGAINST PAPER")
+    }
+    else if (playerSelection === "scissors" && computerRandom === "scissors") {
+    console.log("Both choose scissors, so Draw")
+    }
+    else
+    {
+    console.log( "what is this word? " + playerSelection )
     }
     return result
-}
+    }
